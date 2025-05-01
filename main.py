1277353c6ac8e580ae93f5c337a4218c3f496d72
Ismail <imjogiat@gmail.com>
2025-05-01 07:23:26 -0600

    final working copy

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import requests
+import send_email
 #App to email daily news to a user email address
 #No front end required
 
 #API key and URL for the API- remember to change the url last section to your apikey
 url = """https://newsdata.io/api/1/news?
-apikey=pub_8363350c81b4721e6b54471d1822c0a9ab1ef&q=Honda  
+apikey=pub_8363350c81b4721e6b54471d1822c0a9ab1ef&language=en   
         """
+
 apiKey = "pub_8363350c81b4721e6b54471d1822c0a9ab1ef"
 
 #"gets" the URL and updates that variable in the created request object
@@ -19,12 +21,23 @@ request = requests.get(url)
 #change text variable within the request object to more useful type- dict
 #request object also has a json variable (instead of text variable). this
 #can be used to create a dict structure
-
+# content = request.text
 content = request.json()
-print(content)
+# print(content)
 # # print(content['articles'])
 
+email_message = f"""\
+Subject: News updates from news API
+From: org.imj.yyc@gmail.com
+    """
+
 for article in content['results']:
-    print(article["title"])
-    print(article["description"])
-#
+    # email_message = email_message + "\n\n" + str(article["title"]) + "\n\n" + str(article["description"])
+    email_message = email_message + "\n\n" + str(article["title"]) + "\n\n" + str(article["description"])
+    # print(type(article["title"]))
+    # print(type(article["description"]))
+
+email_message = email_message.encode("utf-8")
+
+# print(email_message)
+send_email.send_email(email_message)
---
+++ send_email.py
@@ -0,0 +1,37 @@
+import os
+import smtplib
+import ssl
+
+
+def send_email(message):
+    #Step 1: Set the host string and port number integer
+    host = "smtp.gmail.com"
+    port = 465
+
+    #Step 2: Get an App password from gmail
+    
+    #Step 3: Set the sending email(username) and password of that account
+    #  and set the receiver email
+
+    #PASSWORD env variable is not authenticating, only string password variable 
+    #authenticates and sends email- not obvious why currently
+    #will leave as string password for now while testing
+    #**REMOVE STRING PASSWORD BEFORE COMMITING AND PUSHING TO GITHUB**
+    username = "org.imj.yyc@gmail.com"
+    password = "jtmy mtxi rutu ykba"
+    # password = os.getenv("PASSWORD")
+
+    receive_email = "imjogiat@gmail.com"
+
+    #Step 4: define a secure SSL context
+    context = ssl.create_default_context()
+
+
+    #Step 5: call SMTP_SSL method from smtplib library with parameters shown. 
+    # This creates a mail server object that can
+    #log in to an email address, send emails and other object methods based on 
+    # method parameters when called
+
+    with smtplib.SMTP_SSL(host, port, context=context) as mailserver:
+        mailserver.login(username, password)
+        mailserver.sendmail(username, receive_email, message)
